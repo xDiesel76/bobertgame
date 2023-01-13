96f6caa208da57858a3bc9078362aff90ed537d2
Diesel <liamzhan2006@gmail.com>
2023-01-13 12:46:22 -0500

    removeded delay

--- Player/Explode.cs
+++ Player/Explode.cs
@@ -14,9 +14,6 @@ public class Explode : MonoBehaviour {
     [SerializeField] GameObject gameoverMenu;
     public int totalDebris = 10;
 
-    void Start() {
-        StartCoroutine(OnExplode());
-    }
     void Update() {
         if (transform.position.y < -105) {
             OnExplode();
@@ -49,8 +46,6 @@ public class Explode : MonoBehaviour {
 
         }
         Destroy(gameObject);
-
-        yield return new WaitForSeconds(2);
 
         gameoverMenu.SetActive(true);
     }
